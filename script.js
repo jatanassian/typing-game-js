766c4239c2b01d2b93388353da8188fcf85c99ed
Julien Atanassian <julien.atanassian@gmail.com>
2020-03-29 16:13:03 -0400

    increment time accordingly to the difficulty level

--- script.js
+++ script.js
@@ -110,7 +110,13 @@ text.addEventListener('input', e => {
     // Clear text input
     text.value = '';
 
-    time += 5;
+    if (difficulty === 'hard') {
+      time += 2;
+    } else if (difficulty === 'medium') {
+      time += 3;
+    } else {
+      time += 5;
+    }
     updateTime();
   }
 });
