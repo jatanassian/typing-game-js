4929117bb6df5285cc3e831418fde7b98da3e4b9
Julien Atanassian <julien.atanassian@gmail.com>
2020-03-29 16:10:59 -0400

    add the difficulty selected to the local storage

--- script.js
+++ script.js
@@ -43,6 +43,12 @@ let score = 0;
 // Init time
 let time = 10;
 
+// Set difficulty to value in localstorage or medium
+let difficulty = localStorage.getItem('difficulty') !== null ? localStorage.getItem('difficulty') : 'medium';
+
+// Set difficulty select value
+difficultySelect.value = localStorage.getItem('difficulty') !== null ? localStorage.getItem('difficulty') : 'medium';
+
 // Focus on text on start
 text.focus();
 
@@ -91,6 +97,8 @@ function gameOver() {
 addWordToDOM();
 
 // Event listeners
+
+// Typing
 text.addEventListener('input', e => {
   const insertedText = e.target.value;
 
@@ -105,4 +113,13 @@ text.addEventListener('input', e => {
     time += 5;
     updateTime();
   }
+});
+
+// Settings
+settingsBtn.addEventListener('click', () => settings.classList.toggle('hide'));
+
+// Settings select
+settingsForm.addEventListener('change', e => {
+  difficulty = e.target.value;
+  localStorage.setItem('difficulty', difficulty);
 });
